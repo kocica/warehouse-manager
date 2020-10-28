af3bc4318bbca73881dc0303736ec5a18a082dc9
Filip Kocica <xkocic01@stud.fit.vutbr.cz>
2020-10-28 19:50:27 +0100

    Fixed utils

--- utils/createExperiment.sh
+++ utils/createExperiment.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 #
 # Simple script to nackup actual working directory for an future experiment
+# Note: Call from project root directory
 #
 
-if [ $# -eq 0 ] || [ -z "$1" ] || [ ! -d "$1" ] then
+if [ $# -eq 0 ] || [ -z "$1" ] || [ -d "$1" ]
+then
     echo "Invalid arguments. Usage: createExperiment.sh <eperiment_dir>"
     exit 1
 fi
 
-cp -r cfg "$1"/cfg
-cp -r data "$1"/data
+mkdir "$1"
+cp -r cfg "$1"
+cp -r data "$1"
