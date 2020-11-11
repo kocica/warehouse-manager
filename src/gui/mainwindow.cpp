3821a47ea31f259aa5d39958e732970aa17b779b
Filip Kocica <xkocic01@stud.fit.vutbr.cz>
2020-11-11 12:23:11 +0100

    Fixed optimizer in UI

--- src/gui/mainwindow.cpp
+++ src/gui/mainwindow.cpp
@@ -401,6 +401,25 @@ namespace whm
             whm::WarehouseLayout_t::getWhLayout().importLocationSlots(args.locationsPath);
             whm::WarehouseLayout_t::getWhLayout().importCustomerOrders(args.ordersPath);
 
+            std::map<std::string, int32_t> heatMap;
+
+            for(auto& order : ::whm::WarehouseLayout_t::getWhLayout().getWhOrders())
+            {
+                for(auto& line : order.getWhOrderLines())
+                {
+                    heatMap[line.getArticle()]++;
+                }
+            }
+
+            int32_t max{ std::numeric_limits<int32_t>::min() };
+            int32_t min{ std::numeric_limits<int32_t>::max() };
+
+            for(auto& m : heatMap)
+            {
+                max = m.second > max ? m.second : max;
+                min = m.second < min ? m.second : min;
+            }
+
             whm::WarehouseOptimizerBase_t* optimizer{ nullptr };
 
             optimizer = new whm::WarehouseOptimizerGA_t{args};
@@ -424,27 +443,6 @@ namespace whm
                                                     }
                                                 }
                                             }
-                                        }
-
-                                        std::map<std::string, int32_t> heatMap;
-
-                                        ::whm::WarehouseLayout_t::getWhLayout().importCustomerOrders(args.ordersPath);
-
-                                        for(auto& order : ::whm::WarehouseLayout_t::getWhLayout().getWhOrders())
-                                        {
-                                            for(auto& line : order.getWhOrderLines())
-                                            {
-                                                heatMap[line.getArticle()]++;
-                                            }
-                                        }
-
-                                        int32_t max{ std::numeric_limits<int32_t>::min() };
-                                        int32_t min{ std::numeric_limits<int32_t>::max() };
-
-                                        for(auto& m : heatMap)
-                                        {
-                                            max = m.second > max ? m.second : max;
-                                            min = m.second < min ? m.second : min;
                                         }
 
                                         for(auto* item : UiWarehouseLayout_t::getWhLayout().getWhItems())
