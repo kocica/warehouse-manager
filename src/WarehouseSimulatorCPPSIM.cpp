0a98ba91fad428d13606a56070e39ba9ecd393ec
Filip Kocica <xkocic01@stud.fit.vutbr.cz>
2020-11-25 15:40:00 +0100

    C++SIM enhanced

--- src/WarehouseSimulatorCPPSIM.cpp
+++ src/WarehouseSimulatorCPPSIM.cpp
@@ -23,46 +23,158 @@
 #include "WarehouseItem.h"
 #include "WarehouseSimulatorCPPSIM.h"
 
-int counter = 0;
-int processedOrders = 0;
-
 namespace whm
 {
-    void WarehouseSimulatorCPPSIM_t::Body()
+    int32_t ordersProcessed{ 0 };
+
+    double WarehouseSimulatorCPPSIM_t::runSimulation()
+    {
+        whm::WarehouseSimulatorCPPSIMInt_t simulator;
+        simulator.Await();
+        simulator.Exit();
+
+        return 0.0;
+    }
+
+    void WarehouseSimulatorCPPSIMInt_t::Body()
     {
         auto* request = new OrderRequestCPPSIM_t(WarehouseLayout_t::getWhLayout(), *this);
         request->Activate();
 
         Scheduler::scheduler().Resume();
 
-        Wait(10000);
+        while(ordersProcessed < 100)
+        {
+            Hold(100);
+        }
+
+        // TODO: Save simulation time
 
         Scheduler::scheduler().reset();
         Scheduler::scheduler().Suspend();
 
         request->terminate();
+
+        if(request)
+        {
+            delete request;
+        }
+
+        Thread::mainResume();
+    }
+
+    void WarehouseSimulatorCPPSIMInt_t::Await()
+    {
+        Resume();
+        Thread::Self()->Suspend();
+    }
+
+    void WarehouseSimulatorCPPSIMInt_t::Exit()
+    {
+        Thread::Exit();
     }
 
     // ================================================================================================================
 
-    OrderRequestCPPSIM_t::OrderRequestCPPSIM_t(WarehouseLayout_t& layout_, WarehouseSimulatorCPPSIM_t& sim_)
+    OrderProcessorCPPSIM_t::OrderProcessorCPPSIM_t(WarehouseOrder_t order_, WarehouseSimulatorCPPSIMInt_t& sim_)
+        : order(order_)
+        , sim(sim_)
+    {
+
+    }
+
+    void OrderProcessorCPPSIM_t::Body()
+    {
+        /*int32_t locationID = 0;
+        int32_t totalDistance = 0;
+        double  totalDuration = 0.0;
+        double  waitDuration = 0.0;
+        double  processDuration = Time;
+
+        const auto& handleFacility = [&](int32_t itemID)
+                                        {
+                                            simlib3::Store* whFacility = sim.getWhItemFacility(itemID);
+
+                                            Enter(*whFacility, 1);
+                                            Wait(waitDuration / sim.getConfig().getAs<double>("simSpeedup"));
+                                            Leave(*whFacility, 1);
+                                        };
+
+        locationID = sim.lookupWhGate(WarehouseItemType_t::E_WAREHOUSE_ENTRANCE)->getWhItemID();
+
+        for(const WarehouseOrderLine_t& orderLine : order)
+        {
+            const std::vector<int32_t>& targetLocations = sim.lookupWhLocations(orderLine.getArticle(), orderLine.getQuantity());
+            const WarehousePathInfo_t* shortestPath = sim.lookupShortestPath(locationID, targetLocations);
+
+            for(const std::pair<int32_t, int32_t>& pathItem : shortestPath->pathToTarget)
+            {
+                waitDuration = pathItem.second / sim.getConfig().getAs<double>("toteSpeed");
+
+                totalDuration += waitDuration;
+                totalDistance += pathItem.second;
+
+                handleFacility(pathItem.first);
+            }
+
+            locationID = shortestPath->targetWhItemID;
+
+            WarehouseItem_t* whLoc = sim.lookupWhLoc(locationID);
+            std::pair<size_t, size_t> slotPos;
+            whLoc->getWhLocationRack()->containsArticle(orderLine.getArticle(), 0, slotPos);
+            static const auto ratio = WarehouseLayout_t::getWhLayout().getRatio();
+            waitDuration = ((slotPos.first  / static_cast<float>(whLoc->getWhLocationRack()->getSlotCountX())) * (whLoc->getW() / ratio) +
+                            (slotPos.second / static_cast<float>(whLoc->getWhLocationRack()->getSlotCountY())) * (whLoc->getH() / ratio)) / sim.getConfig().getAs<double>("workerSpeed");
+
+            totalDuration += waitDuration;
+            handleFacility(locationID);
+        }
+
+        int32_t dispatchID = sim.lookupWhGate(WarehouseItemType_t::E_WAREHOUSE_DISPATCH)->getWhItemID();
+
+        const WarehousePathInfo_t* shortestPath = sim.lookupShortestPath(locationID, std::vector<int32_t>{ dispatchID });
+
+        for(const std::pair<int32_t, int32_t>& pathItem : shortestPath->pathToTarget)
+        {
+            waitDuration = pathItem.second / sim.getConfig().getAs<double>("toteSpeed");
+
+            totalDuration += waitDuration;
+            totalDistance += pathItem.second;
+
+            handleFacility(pathItem.first);
+        }
+
+        locationID = dispatchID;
+
+        waitDuration = (60 / sim.getConfig().getAs<int32_t>("totesPerMin"));
+
+        totalDuration += waitDuration;
+
+        handleFacility(locationID);
+
+        sim.orderFinished(Time - processDuration, totalDuration, totalDistance);*/
+    }
+
+    OrderRequestCPPSIM_t::OrderRequestCPPSIM_t(WarehouseLayout_t& layout_, WarehouseSimulatorCPPSIMInt_t& sim_)
         : layout(layout_)
         , sim(sim_)
         , it(layout.getWhOrders().begin())
     {
-        c = counter++;
+
     }
 
     void OrderRequestCPPSIM_t::Body()
     {
-        for(;processedOrders < 100;)
+        for(;;)
         {
-            processedOrders++;
+            (new OrderProcessorCPPSIM_t(*it, sim))->Activate();
 
-            std::cerr << "Hello C++SIM: " << c << std::endl;
+            ordersProcessed++;
 
-            Hold(100.0);
-            // Order processor call
+            if(++it != layout.getWhOrders().end())
+            {
+                Hold(1.0);
+            }
         }
     }
 }
--- src/WarehouseSimulatorCPPSIM.h
+++ src/WarehouseSimulatorCPPSIM.h
@@ -37,25 +37,101 @@
 
 namespace whm
 {
-    class WarehouseSimulatorCPPSIM_t : public Entity
+    class WarehouseSimulatorCPPSIM_t
+    {
+        public:
+            /*WarehouseSimulatorCPPSIM_t();
+            ~WarehouseSimulatorCPPSIM_t();*/
+
+            double runSimulation();
+            /*void orderFinished(double, double, int32_t);
+
+            void printStats(bool);
+
+            bool& optimizationModeActive();
+
+            ConfigParser_t& getConfig();
+            void setConfig(const ConfigParser_t&);
+
+            utils::WhmArgs_t getArguments() const;
+            void setArguments(const utils::WhmArgs_t&);
+
+#           ifdef WHM_GUI
+            using UiCallback_t = std::function<void(double, bool)>;
+            void setUiCallback(UiCallback_t);
+#           endif
+
+            simlib3::Store* getWhItemFacility(int32_t);
+
+            WarehouseItem_t* lookupWhLoc(int32_t);
+            WarehouseItem_t* lookupWhGate(const WarehouseItemType_t&);
+            std::vector<int32_t> lookupWhLocations(const std::string&, int32_t);
+            WarehousePathInfo_t* lookupShortestPath(int32_t, const std::vector<int32_t>&);*/
+
+            static void init()
+            {
+                Thread::Initialize();
+            }
+
+        /*protected:
+            void clearSimulation();
+            void preprocessOrders();
+            void prepareWhSimulation();
+
+        private:
+            bool stats;
+            bool optimizationMode;
+            bool multipleExperiments;
+
+            size_t ordersFinished{ 0 };
+
+#           ifdef WHM_GUI
+            UiCallback_t uiCallback;
+#           endif
+
+            ConfigParser_t cfg;
+            utils::WhmArgs_t args;
+
+            WarehouseLayout_t& whLayout;
+            WarehousePathFinder_t* whPathFinder;
+
+            std::map<int32_t, simlib3::Store*> whFacilities;*/
+    };
+
+    class WarehouseSimulatorCPPSIMInt_t : public Process
     {
         public:
             void Body() override;
+
+            void Exit();
+            void Await();
+    };
+
+    class OrderProcessorCPPSIM_t : public Process
+    {
+        public:
+            OrderProcessorCPPSIM_t(WarehouseOrder_t, WarehouseSimulatorCPPSIMInt_t&);
+
+        protected:
+            void Body() override;
+
+        private:
+            WarehouseOrder_t order;
+            WarehouseSimulatorCPPSIMInt_t& sim;
     };
 
     class OrderRequestCPPSIM_t : public Process
     {
         public:
-            OrderRequestCPPSIM_t(WarehouseLayout_t&, WarehouseSimulatorCPPSIM_t&);
+            OrderRequestCPPSIM_t(WarehouseLayout_t&, WarehouseSimulatorCPPSIMInt_t&);
 
         protected:
             void Body() override;
 
         private:
             WarehouseLayout_t& layout;
-            WarehouseSimulatorCPPSIM_t& sim;
+            WarehouseSimulatorCPPSIMInt_t& sim;
             std::vector<WarehouseOrder_t>::const_iterator it;
-            int c;
     };
 }
 
