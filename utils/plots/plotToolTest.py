1249472b7bc9eebcbc4d86f9e655cc60519570d2
Filip Kocica <xkocic01@stud.fit.vutbr.cz>
2021-01-02 13:55:28 +0100

    data

--- utils/plots/plotToolTest.py
+++ utils/plots/plotToolTest.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 algs = ('GA', 'ABC', 'DE', 'SLAP', 'PSO', 'RAND', 'Puvodni')
 
 alg_ids = np.arange(len(algs))
-performance = [9848, 11060, 11264, 11749, 12076, 16903, 18000]
+performance = [9247, 11060, 11264, 11749, 12076, 16903, 18000]
 
 barlist = plt.bar(alg_ids, performance, align='center')
 plt.xticks(alg_ids, algs)
